57abce34bf6e614c27ee6034184add3441e944cf
Ravindrakale <ravind999@gmail.com>
2023-07-07 15:55:21 +0530

    Assignment 02

--- Chapter 02 Igniting our app/Assignment 02/app.js
+++ Chapter 02 Igniting our app/Assignment 02/app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const eleHeader1 = React.createElement("h1", { key: "h1" }, "Header One");
 const eleHeader2 = React.createElement("h1", { key: "h2" }, "Header Two");
