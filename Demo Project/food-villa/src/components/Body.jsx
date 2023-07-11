cee95b8233cff69550c9fc0278e2832c1b174eb0
Ravindrakale <ravind999@gmail.com>
2023-07-11 18:54:51 +0530

    Search Bar implementation

--- Demo Project/food-villa/src/components/Body.jsx
+++ Demo Project/food-villa/src/components/Body.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Restaurants from "../data/restaurants.json";
 
 function Body() {
+  const [searchTxt, setSearchTxt] = useState("");
+  const [restaurants, setRestaurants] = useState(Restaurants);
+
+  const filterRestaurants = () => {
+    if (searchTxt) {
+      setRestaurants(
+        Restaurants.filter((rest) => rest?.data?.name?.toLowerCase().includes(searchTxt.toLowerCase()))
+      );
+    } else {
+      setRestaurants(Restaurants);
+    }
+  };
+
+  const onSearchChange = (e) => {
+    setSearchTxt(e.target.value);
+  };
+
   return (
-    <div className="app-body">
-      {Restaurants.map((rest) => {
-        return <RestaurantCard key={rest.data.uuid} {...rest.data} />;
-      })}
-    </div>
+    <>
+      <div className="mt-10">
+        <input
+          className="search-txt"
+          type="text"
+          placeholder="Search Restaurants"
+          value={searchTxt}
+          onChange={onSearchChange}
+        />
+        <button onClick={filterRestaurants}>Search</button>
+      </div>
+      <div className="app-body">
+        {restaurants?.length ? (
+          restaurants.map((rest) => {
+            return <RestaurantCard key={rest.data.uuid} {...rest.data} />;
+          })
+        ) : (
+          <div className="mt-10">No Restaurant Found.</div>
+        )}
+      </div>
+    </>
   );
 }
 
