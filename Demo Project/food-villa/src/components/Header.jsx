c7119a9509c8f9308dda4a02377e4ebdb523ed11
Ravindrakale <ravind999@gmail.com>
2023-07-14 19:48:11 +0530

    Logged in functionalicty

--- Demo Project/food-villa/src/components/Header.jsx
+++ Demo Project/food-villa/src/components/Header.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import mainLogo from "../../public/images/food-villa.jpg";
 
 function Header() {
-console.log("Header Render");
-
+  const [userLoggedin, setUserLoggedin] = useState(false);
   return (
     <div className="app-header">
       <div>
@@ -17,6 +16,13 @@ console.log("Header Render");
           <li>About</li>
           <li>Contact</li>
         </ul>
+      </div>
+      <div>
+        {userLoggedin ? (
+          <button onClick={() => setUserLoggedin(false)}>Log out</button>
+        ) : (
+          <button onClick={() => setUserLoggedin(true)}>Log In</button>
+        )}
       </div>
     </div>
   );
